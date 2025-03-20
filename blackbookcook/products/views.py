4c914ee8a3d2b4dbc3eb81b49d6b27c91db841b3
Danis Art <nikita.efremov1412@gmail.com>
2025-03-20 21:30:59 +0300

    Добавил передачу категорий продуктов

--- blackbookcook/products/views.py
+++ blackbookcook/products/views.py
@@ -7,7 +7,9 @@ class ProductsView(View):
     def get(self, request):
         form = ProductForm()
         products_list = Product.objects.all()
-        return render(request, 'products/products.html', {'form': form, 'products': products_list})
+        categories = Product.objects.values_list('categories', flat=True).exclude(categories='').distinct()
+        print(categories)
+        return render(request, 'products/products.html', {'form': form, 'products': products_list, 'categories': categories})
 
     def post(self, request):
         form = ProductForm(request.POST)
